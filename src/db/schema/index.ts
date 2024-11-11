8f9705031326d621b54c460cfa117882ba29aa42
Luis Carlos [FRONT] <luisestrela.bol951@gmail.com>
2024-11-11 19:14:56 -0300

    feat: add base config drizzle

---
+++ src/db/schema/index.ts
@@ -0,0 +1,9 @@
+import { serial, text } from 'drizzle-orm/pg-core';
+import { integer, pgTable } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users_table', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  age: integer('age').notNull(),
+  email: text('email').notNull().unique(),
+});
