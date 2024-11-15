9bcac4d7ce67bf004640a1e228a31c976104c634
Luis Carlos [FRONT] <luisestrela.bol951@gmail.com>
2024-11-15 15:05:11 -0300

    feat: add employee field

--- src/db/models/users.ts
+++ src/db/models/users.ts
@@ -3,6 +3,12 @@ import { customersTable, usersTable } from '../schema';
 
 export type User = InferSelectModel<typeof usersTable>;
 export type Customer = InferSelectModel<typeof customersTable>;
+
+export type Employee = {
+  id: string;
+  name: string;
+  email: string;
+};
 export type CustomerAddress = {
   farmName: string;
   city: string;
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -6,7 +6,7 @@ import {
   json,
   timestamp,
 } from 'drizzle-orm/pg-core';
-import { CustomerAddress } from '../models/users';
+import { CustomerAddress, Employee } from '../models/users';
 
 const createdAt = timestamp('created_at').defaultNow().notNull();
 const updatedAt = timestamp('updated_at')
@@ -19,6 +19,7 @@ export const usersTable = pgTable('users', {
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
   password: varchar('password').notNull(),
+  employees: json('employees').$type<Employee[]>().notNull(),
   createdAt,
   updatedAt,
 });
@@ -34,4 +35,16 @@ export const customersTable = pgTable('customers', {
     .notNull(),
   createdAt,
   updatedAt,
+});
+
+export const procedures = pgTable('procedures', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  description: text('description').notNull(),
+  employee: json('employee').$type<Employee>().notNull(),
+  createdAt,
+  updatedAt,
+  customerId: uuid('customer_id')
+    .references(() => customersTable.id)
+    .notNull(),
 });
